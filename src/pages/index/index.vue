d40732af4ded0066bbae396decd588e9aec724dc
yingzi <GTyingzi@gmail.com>
2025-03-04 22:37:06 +0800

    feat:轮播图 & 前台分类 & 猜你喜欢 & 首页

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,12 +1,105 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import { onLoad } from '@dcloudio/uni-app'
 import CustomNavbar from './components/CustomNavbar.vue'
+import CategoryPanel from './components/CategoryPanel.vue'
+import HotPanel from './HotPanel.vue'
+import { getHomeBannerAPI, getHomeCategoryAPI, getHomeHotAPI } from '@/services/home'
+import type { BannerItem, CategoryItem, HotItem } from '@/types/home'
+import type { XtxGuessInstance } from '@/types/components'
+import PageSkeleton from './components/PageSkeleton.vue'
+
+// 获取轮播图数据
+const bannerList = ref<BannerItem[]>([])
+const getHomeBannerData = async () => {
+  const res = await getHomeBannerAPI()
+  console.log(res)
+  bannerList.value = res.result
+}
+// 获取前台分类数据
+const categoryList = ref<CategoryItem[]>([])
+const getHomeCategoryData = async () => {
+  const res = await getHomeCategoryAPI()
+  console.log(res)
+  categoryList.value = res.result
+}
+
+// 获取热门推荐数据
+const hotList = ref<HotItem[]>([])
+const getHomeHotData = async () => {
+  const res = await getHomeHotAPI()
+  hotList.value = res.result
+}
+
+const isLoading = ref(false)
+
+// 页面加载
+onLoad(async () => {
+  isLoading.value = true
+  await Promise.all([getHomeBannerData(), getHomeCategoryData(), getHomeHotData()])
+  isLoading.value = false
+})
+
+// 获取猜你喜欢组件实例
+const guessRef = ref<XtxGuessInstance>()
+// 滚动触底
+const onScrolltolower = () => {
+  // console.log('滚动触底了')
+  guessRef.value?.getMore()
+}
+// 自定义下拉刷新被触发
+const isTriggered = ref(false)
+const onRefresherrefresh = async () => {
+  isTriggered.value = true
+  // await getHomeBannerData()
+  // await getHomeCategoryData()
+  // await getHomeHotData()
+  // 重置猜你喜欢组件数据
+  guessRef.value?.resetData()
+  await Promise.all([
+    getHomeBannerData(),
+    getHomeCategoryData(),
+    getHomeHotData(),
+    guessRef.value?.getMore(),
+  ])
+  isTriggered.value = false
+}
 </script>
 
 <template>
+  <!-- 自定义导航栏 -->
   <CustomNavbar />
-  <view class="index">index</view>
+  <scroll-view
+    refresher-enabled
+    @refresherrefresh="onRefresherrefresh"
+    @scrolltolower="onScrolltolower"
+    :refresher-triggered="isTriggered"
+    class="scroll-view"
+    scroll-y
+  >
+    <!-- 骨架图 -->
+    <PageSkeleton v-if="isLoading" />
+    <template v-else>
+      <!-- 自定义轮播图 -->
+      <XtxSwiper :list="bannerList" />
+      <!-- 分类面板 -->
+      <CategoryPanel :list="categoryList" />
+      <!-- 热门推荐 -->
+      <HotPanel :list="hotList" />
+      <!-- 猜你喜欢 -->
+      <XtxGuess ref="guessRef" />
+    </template>
+  </scroll-view>
 </template>
 
 <style lang="scss">
-//
+page {
+  background-color: #f7f7f7;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.scroll-view {
+  flex: 1;
+}
 </style>
--- src/types/components.d.ts
+++ src/types/components.d.ts
@@ -1,10 +1,14 @@
 /// <reference types="@dcloudio/types" />
+import XtxSwiper from './XtxSwiper.vue'
+import XtxGuess from '@/components/XtxGuess.vue'
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     button: typeof import('@uni-helper/uni-app-types')['Button']
     view: typeof import('@uni-helper/uni-app-types')['View']
+    XtxSwiper: typeof XtxSwiper
+    XtxGuess: typeof XtxGuess
   }
 }
-
-export {}
+// 组件实例类型
+export type XtxGuessInstance = InstanceType<typeof XtxGuess>
